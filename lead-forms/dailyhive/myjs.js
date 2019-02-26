94ad2a89ee30833d7ce06006e9c444ef0143125b
salilk <salil.kalia@gmail.com>
2019-02-27 02:14:36 +0530

    dailyhive lead-form updated.

---
+++ lead-forms/dailyhive/myjs.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+    $("form").submit( () => {
+        if($("form").valid()) {
+            const thankYouMsg = 'Thanks for filling the form!';
+            $('.post_newsletter').html(thankYouMsg).addClass('flip-vertical');
+            window.parent.postMessage(JSON.stringify({tc: true, formSubmission: true, formData: $('form').serialize()}), '*');
+        }
+        return false;
+    });
+
+    // alias required to cRequired with new message
+    $.validator.addMethod("cRequired", $.validator.methods.required,
+        "This field is required");
+    $.validator.addMethod("cEmail", $.validator.methods.email,
+        "Please enter valid Email");
+    $.validator.addMethod("cMobile", (phone) => {
+        var regex = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{0,})/;
+        return regex.test(phone);
+    }, "Please enter valid Mobile No.");
+    $.validator.addClassRules("required", { cRequired: true});
+    $.validator.addClassRules("email", { cEmail: true});
+    $.validator.addClassRules("mobile", { cMobile: true});
+    $("form").validate({
+        submitHandler: function(form) {
+            form.submit();
+        },
+        errorPlacement: function(error, element) {
+            element.before(error)
+        }
+    });
+});
+
+
