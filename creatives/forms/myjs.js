70bcaad0c27b9728923cacfbd8d4c275873cca06
salilk <salil.kalia@gmail.com>
2019-09-14 11:25:41 +0530

    study-abroad form and myjs updated.

--- creatives/forms/myjs.js
+++ creatives/forms/myjs.js
@@ -11,19 +11,22 @@ $(document).ready(() => {
 
     // alias required to cRequired with new message
     $.validator.addMethod("cRequired", $.validator.methods.required,
-        "This field is required");
+        "This field is required.");
     $.validator.addMethod("cEmail", $.validator.methods.email,
-        "Please enter valid Email");
+        "Enter valid email address.");
     $.validator.addMethod("cMobile", (phone) => {
-        var regex = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{0,})/;
-        return regex.test(phone);
-    }, "Please enter valid Mobile No.");
+        /*var regex = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{0,})/;
+        return regex.test(phone) || (phone && phone.length >= 10);*/
+        var regex = /^[6-9]\d{9}$/; //Indian mobile number
+        return regex.test(phone);// || (phone && phone.length >= 10);
+    }, "Enter 10 digits mobile number.");
     $.validator.addClassRules("required", { cRequired: true});
     $.validator.addClassRules("email", { cEmail: true});
     $.validator.addClassRules("mobile", { cMobile: true});
+    $.validator.addClassRules("email-required", { cRequired: true, cEmail: true});
+    $.validator.addClassRules("mobile-required", { cRequired: true, cMobile: true});
     $("form").validate({
         submitHandler: function(form) {
-            form.submit();
         },
         errorPlacement: function(error, element) {
             element.before(error)
