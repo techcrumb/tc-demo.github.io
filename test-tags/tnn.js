593ad69ea880e640b5dd88292a6a3f3b91602484
Dheeraj Madaan <dheerajmadaangrp@gmail.com>
2020-02-08 22:29:55 +0530

    Added cahnges for times.

--- test-tags/tnn.js
+++ test-tags/tnn.js
@@ -1,7 +1,10 @@
 (function (t, i, s, a) {
     var c = 'script', src = 'https://tc-vai.web.app', E = window, m = document;
+    try {
+        m = E.top.document || m; E = E.top.document ? E.top : E;
+    } catch (e) {}
     E[t] = E[t] || {};
-    b = m.getElementsByTagName(c)[0];
+    var b = m.getElementsByTagName(c)[0];
     m = m.createElement(c);
     document.getElementById('tcpubslot').style.width = window.innerWidth + 'px';
     document.getElementById('tcpubslot').style.height = window.innerHeight + 'px';
@@ -12,6 +15,21 @@
     m.async = 1;
     m.src = src + "/tc-initForm.js";
     b.parentNode.insertBefore(m, b)
+    if(!E.getElementById(i.adSlot)) {
+        var slot = '', slotEle = '';
+        if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            slot = '.storyContent > p, .photo_data .photo_detail_cap > p';
+            slotEle = document.querySelectorAll(slot)[0];
+        }else{
+            slot = '.artical-description > p';
+            slotEle = document.querySelectorAll(slot)[1];
+        }
+        var newElement = document.createElement('div');
+        newElement.id = 'tcpubslot';
+        newElement.style.width = E.innerWidth + 'px';
+        slotEle.parentNode.insertBefore(newElement, slotEle.nextSibling);
+
+    }
 })("tcEmbed", {
     ed: "jCsxHFDKTlmnRFXDB49wP6eyGA8zqZUuYZWd0IlpYVtl+cDrrzokaToijBolaV4zZ3t37SpuY3HvAf5A1k+F0Q==",
     pId: "",
