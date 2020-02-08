94b972b00ce169da8dd4c267a6756c7a90e238e4
Dheeraj Madaan <dheerajmadaangrp@gmail.com>
2020-02-08 23:41:41 +0530

    Added cahnges for times.

--- test-tags/tnn.js
+++ test-tags/tnn.js
@@ -5,30 +5,31 @@
     } catch (e) {}
     E[t] = E[t] || {};
     var b = m.getElementsByTagName(c)[0];
-    m = m.createElement(c);
-    document.getElementById('tcpubslot').style.width = window.innerWidth + 'px';
-    document.getElementById('tcpubslot').style.height = window.innerHeight + 'px';
+    var se = m.createElement(c);
     E[t].i = i;
     E[t].a = a;
     E[t].s = s;
     E[t].src = src;
-    m.async = 1;
-    m.src = src + "/tc-initForm.js";
-    b.parentNode.insertBefore(m, b)
-    if(!E.getElementById(i.adSlot)) {
+    se.async = 1;
+    se.src = src + "/tc-initForm.js";
+    b.parentNode.insertBefore(se, b)
+    if(!m.getElementById(i.adSlot)) {
         var slot = '', slotEle = '';
         if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
             slot = '.storyContent > p, .photo_data .photo_detail_cap > p';
-            slotEle = document.querySelectorAll(slot)[0];
+            slotEle = m.querySelectorAll(slot)[0];
         }else{
             slot = '.artical-description > p';
-            slotEle = document.querySelectorAll(slot)[1];
+            slotEle = m.querySelectorAll(slot)[1];
+            if (!slotEle) {
+                slot = '.storyContent > p, .photo_data .photo_detail_cap > p';
+                slotEle = m.querySelectorAll(slot)[0];
+            }
         }
-        var newElement = document.createElement('div');
+        var newElement = m.createElement('div');
         newElement.id = 'tcpubslot';
         newElement.style.width = E.innerWidth + 'px';
         slotEle.parentNode.insertBefore(newElement, slotEle.nextSibling);
-
     }
 })("tcEmbed", {
     ed: "jCsxHFDKTlmnRFXDB49wP6eyGA8zqZUuYZWd0IlpYVtl+cDrrzokaToijBolaV4zZ3t37SpuY3HvAf5A1k+F0Q==",
